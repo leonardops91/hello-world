6d23e2047585e653e5345bc13d839b403531f313
Leonardo Souza <55004732+leonardops91@users.noreply.github.com>
2022-12-14 14:46:48 +0000

    feat: emailForm responsivity backdrop-blur

--- src/components/contactForm/index.tsx
+++ src/components/contactForm/index.tsx
@@ -3,8 +3,9 @@ import {Text} from '../textComponent'
 
 
 export default function ContactForm() {
+    const theme = localStorage.getItem('theme') || 'dark'
     return (
-      <div className="flex flex-col gap-2 p-4 max-w-xl rounded-md backdrop-blur-md bg-gray-100 bg-opacity-30">
+      <div className={`flex flex-col gap-2 p-4 max-w-xl rounded-md ${theme === "dark" ? 'backdrop-blur-sm' : 'backdrop-blur-md'} bg-gray-100 bg-opacity-30`}>
         <Text content="What about to send me a message? Let's talk" variant='subtitle' />
         <form className="flex gap-4 flex-col items-center">
             <InputText type='text' id='name' label='Name' required />
