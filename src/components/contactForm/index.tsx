f8f2fa004fe19443d3a3d81f86c7cbbba1beb31a
Leonardo Souza <55004732+leonardops91@users.noreply.github.com>
2022-12-14 14:23:17 +0000

    feat: emailForm responsivity

--- src/components/contactForm/index.tsx
+++ src/components/contactForm/index.tsx
@@ -4,13 +4,13 @@ import {Text} from '../textComponent'
 
 export default function ContactForm() {
     return (
-      <div className="flex flex-col gap-2 p-4 border-2 max-w-xl rounded-md">
+      <div className="flex flex-col gap-2 p-4 max-w-xl rounded-md backdrop-blur-md bg-gray-100 bg-opacity-30">
         <Text content="What about to send me a message? Let's talk" variant='subtitle' />
         <form className="flex gap-4 flex-col items-center">
             <InputText type='text' id='name' label='Name' required />
-            <InputText type='text' id='email' label='E-mail' required />
+            <InputText type='email' id='email' label='E-mail' required />
             <InputText type='textarea' id='message' label='Message' required />
-          <button>Enviar</button>
+          <button className="border-2 hover:contrast-150 py-1 px-4 rounded-md">Enviar</button>
         </form>
       </div>
     );
--- src/components/inputText/index.tsx
+++ src/components/inputText/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 
 type InputTextProps = {
     type: string
@@ -8,26 +9,30 @@ type InputTextProps = {
 }
 
 export default function InputText(props: InputTextProps){
+    const [value, setValue] = useState('')
 
     return (
-      <fieldset className="w-full max-w-lg relative">
-        <label htmlFor={props.id} className='absolute left-3 top-2 z-10'>{props.label}</label>
+      <fieldset className="group w-full max-w-lg relative "> 
         {props.type === "textarea" ? (
           <textarea
             name={props.id}
-            id={props.id}
-            required
-            className={`bg-black opacity-70 backdrop:blur-lg p-2 resize-none h-44 rounded-sm w-full ${props.classNames}`}
+            id={props.id} 
+            onChange={e => setValue(e.target.value)}
+            required={props.required}
+            className={`bg-black peer opacity-70 backdrop:blur-lg p-2 resize-none h-44 rounded-sm w-full ${props.classNames}`}
           />
         ) : (
           <input
             type={props.type}
             name={props.id}
-            id={props.id}
+            id={props.id}  
+            onChange={e => setValue(e.target.value)}
             required={props.required}
-            className={`bg-black opacity-70 backdrop:blur-lg p-2 rounded-sm w-full ${props.classNames}`}
+            className={`bg-black peer opacity-70 backdrop:blur-lg p-2 rounded-sm w-full ${props.classNames}`}
           />
         )}
+        <label htmlFor={props.id} className={`absolute left-3 top-2 transition-all px-2 rounded-sm z-10 peer-focus:text-xs peer-focus:-translate-y-4 peer-focus:text-gray-900 peer-focus:bg-white ${value && 'text-xs -translate-y-4 text-gray-900 bg-white'}`}>{props.label}</label>
+
       </fieldset>
     );
 }
